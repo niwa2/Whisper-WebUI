6adf799abb07e89d0078d19e3b5c221c49f42ae2
niwa2 <niwa2@gmx.de>
2025-01-18 15:57:13 +0100

    Update default parameters for more robust transcription

--- modules/utils/constants.py
+++ modules/utils/constants.py
@@ -2,5 +2,5 @@ from gradio_i18n import Translate, gettext as _
 
 AUTOMATIC_DETECTION = _("Automatic Detection")
 GRADIO_NONE_STR = ""
-GRADIO_NONE_NUMBER_MAX = 9999
+GRADIO_NONE_NUMBER_MAX = 999999
 GRADIO_NONE_NUMBER_MIN = 0
